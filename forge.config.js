5acf29622f0d0fce9161248be50fad0a1586e6b4
marisukukise <109931938+marisukukise@users.noreply.github.com>
2022-12-25 00:40:48 +0100

    Updated electron-forge to version 6

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,22 @@
 module.exports = {
-  makers: [{
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
       name: '@electron-forge/maker-squirrel',
-      config: {
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD,
-      },
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {
-        options: {
-          maintainer: 'marisukukise',
-          homepage: 'https://github.com/marisukukise/japReader'
-        }
-      }
+      config: {},
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {
-        options: {
-          homepage: 'https://github.com/marisukukise/japReader'
-        }
-      }
-    }, 
-    {
-      name: '@electron-forge/maker-zip'
-    }
+      config: {},
+    },
   ],
 };
