b5e19e406f6f8464833a78e41387f045897d3b05
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-05-18 04:30:57 +0200

    Added conditionals for whether to use DeepL Api or not

--- src/boxes/deepl/script.js
+++ src/boxes/deepl/script.js
@@ -11,33 +11,45 @@ window.addEventListener('DOMContentLoaded', () => {
   ipcRenderer.on('translateWithDeepL', (event, text) => {
     const currentText = text.replace(/…+/, '…').replace(/・+/g, '…');
     ipcRenderer.send('translateNotification');
-    document.location.href = `https://www.deepl.com/translator#ja/en/${currentText}`;
+
+    if(useDeepLApi){
+
+    }
+    else{
+      document.location.href = `https://www.deepl.com/translator#ja/en/${currentText}`;
+    }
   });
 
-  const targetNode = document.querySelector('div[aria-labelledby="translation-results-heading"]');
-  const sourceNode = document.querySelector('div[aria-labelledby="translation-source-heading"]');
-  const config = { childList: true };
-  const callback = () => {
-    if (targetNode.textContent) {
-      const deeplText = [...targetNode.children].map(x => x.textContent).join(" ");
-      const japaneseText = [...sourceNode.children].map(x => x.textContent).join(" ");
-      ipcRenderer.send('showTranslation', deeplText, japaneseText);
-    }
-  };
+    if(useDeepLApi){
 
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
-
-  const connectionCheck = setTimeout(() => {
-    if (document.querySelector('.dl_body').length !== 0) {
-      ipcRenderer.send('deepLConnected');
-      clearInterval(connectionCheck);
     }
-  }, 500);
+    else{
+      const targetNode = document.querySelector('div[aria-labelledby="translation-results-heading"]');
+      const sourceNode = document.querySelector('div[aria-labelledby="translation-source-heading"]');
+      const config = { childList: true };
+      const callback = () => {
+        if (targetNode.textContent) {
+          const deeplText = [...targetNode.children].map(x => x.textContent).join(" ");
+          const japaneseText = [...sourceNode.children].map(x => x.textContent).join(" ");
+          ipcRenderer.send('showTranslation', deeplText, japaneseText);
+        }
+      };
+
+      const observer = new MutationObserver(callback);
+      observer.observe(targetNode, config);
+
+      const connectionCheck = setTimeout(() => {
+        if (document.querySelector('.dl_body').length !== 0) {
+          ipcRenderer.send('deepLConnected');
+          clearInterval(connectionCheck);
+        }
+      }, 500);
+
+      setTimeout(() => {
+        if (document.body.children.length === 0) {
+          ipcRenderer.send('deepLConnectionError');
+        }
+      }, 10000);
+}
 });
 
-setTimeout(() => {
-  if (document.body.children.length === 0) {
-    ipcRenderer.send('deepLConnectionError');
-  }
-}, 10000);
