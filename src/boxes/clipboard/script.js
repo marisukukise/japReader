c8ca2ec9006f284a4101f626b32a02bdeb173199
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-06-13 00:41:55 +0200

    Added some robustness to clipboard reading mechanism

--- src/boxes/clipboard/script.js
+++ src/boxes/clipboard/script.js
@@ -5,9 +5,12 @@ const os = require('os');
 
 const charLimit = 90;
 let clipboardText = '';
+let TOTAL_READS = 0;
+let MISREADINGS = 0;
 
 const formatText = (text) => {
-  text = text
+  console.log(text)
+  text = text.trim()
     // remove symbols (heart, star, etc.)
     .replace(/[\u22c0-\u266b]/g, '')
     // turn half-width digits into full-width
@@ -18,20 +21,45 @@ const formatText = (text) => {
   return text;
 }
 
-const handleChange = () => {
-  console.log(clipboard.readText())
-  clipboardContent = clipboard.readText()
-  if (clipboardContent.includes("???")) {
-    console.error("Clipboard content:", clipboardContent);
-  } else {
-    console.log("Clipboard content:", clipboardContent);
+const handleChange = (operationID) => {
+ console.info("<Operation ", operationID, ">")
+  console.info("Detected clipboard change @ ", Date.now())
+  console.info("Start readings @ ",Date.now())
+  let clipboard_reads = [];
+  // Read clipboard 20 times in a row and get majority value
+  // Because sometimes at low frequency (exact reason unknown)
+  // Clipboard reads return empty strings.
+  for (var i = 0; i < 20; i++) {
+    clipboard_reads[i] = clipboard.readText();
+    TOTAL_READS += 1;
   }
+  console.info("Stop readings @ ",Date.now())
+  console.info("Start finding majority @ ",Date.now())
+
+  const counts = {};
+  for (const e of clipboard_reads) counts[e] = counts[e] ? counts[e] + 1 : 1;
+
+  text = '';
   
-  let text = clipboard.readText().trim();
-  console.log("Trimmed text: ",text)
+  console.debug(counts);
+
+  if (Object.keys(counts).length != 1){
+    const majority_string = Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b);
+    text = majority_string;
+    console.debug(majority_string);
+    MISREADINGS += 20-counts[majority_string];
+    console.error(`DETECTED ${20-counts[majority_string]} MISREADINGS. ${((MISREADINGS/TOTAL_READS)*100).toFixed(3)}% OF TOTAL READS WERE MISREADINGS.`)
+  }
+  else {
+    text = Object.keys(counts)[0];
+  }
+  console.info("Stop finding majority @ ",Date.now())
+  console.table([TOTAL_READS, MISREADINGS])
 
   text = formatText(text);
-  console.log("Formatted text: ",text)
+  console.log("Copied text: ",text);
+
+  console.info("</Operation ", operationID, ">")
 
   if (text !== clipboardText && /[一-龯]|[ぁ-んァ-ン]|…/.test(text)) {
     clipboardText = text;
@@ -56,7 +84,6 @@ window.addEventListener('DOMContentLoaded', () => {
   */
   clipboardListener.startListening();
   clipboardListener.on('change', () => {
-    console.log('\nCLIPBOARD CHANGE DETECTED');
-    handleChange();
+    handleChange(Math.random());
   });
 });
