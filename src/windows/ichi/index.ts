9a00f3c9322b07e5931219e1dfc0ebe101223b22
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-06-28 22:04:50 +0200

    Emptied unused files

--- src/windows/ichi/index.ts
+++ src/windows/ichi/index.ts
@@ -1,10 +1,1 @@
-// This is the TypeScript file of the ichi window.
-
-import log from 'electron-log/renderer';
-log.silly('Log from the ichi process');
-
-window.addEventListener('DOMContentLoaded', () => {
-  window.api.log("yo from ichi");
-  console.log("yo")
-});
-
+// This file is not being used but must be here for the webpack configuration in forge.config.ts
