ad31e013faa3a05d7a597b82f44330e9a8b4cb51
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-06-29 01:15:42 +0200

    Copied DeepL window logic

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@
   Available variables: 
   - JAPREADER_LOGS (with log level value: https://github.com/megahertz/electron-log#log-levels)
   - JAPREADER_SHOW_ALL_WINDOWS ("true" for yes, otherwise no)
-*/  
+*/
 
 import { dialog, app, BrowserWindow, globalShortcut, ipcMain } from 'electron';
 import 'dotenv/config';
 import log from 'electron-log';
 const path = require('path');
+
+const useDeepLApi = false;
+const useDeepL = true;
+const useReader = true;
 
 log.initialize({ preload: true });
 
@@ -30,7 +34,7 @@ log.errorHandler.startCatching({
   onError({ createIssue, error, processType, versions }) {
     if (processType === 'renderer') {
       return;
-    } 
+    }
     dialog.showMessageBox({
       title: 'An error occurred',
       message: error.message,
@@ -38,17 +42,17 @@ log.errorHandler.startCatching({
       type: 'error',
       buttons: ['Ignore', 'Report on github', 'Exit'],
     }).then((result) => {
-        if (result.response === 1) {
-          createIssue('https://github.com/marisukukise/japReader/issues/new', {
-            title: `[ERROR] Report for ${versions.app}`,
-            body: 'Error message:\n```' + error.stack + '\n```\n\n' + `OS: ${versions.os}\n\nDescription of what lead to the error:\n\n\n\nAdditional info (optional):\n\n`
-          });
-          return;
-        }
-        if (result.response === 2) {
-          app.quit();
-        }
-      });
+      if (result.response === 1) {
+        createIssue('https://github.com/marisukukise/japReader/issues/new', {
+          title: `[ERROR] Report for ${versions.app}`,
+          body: 'Error message:\n```' + error.stack + '\n```\n\n' + `OS: ${versions.os}\n\nDescription of what lead to the error:\n\n\n\nAdditional info (optional):\n\n`
+        });
+        return;
+      }
+      if (result.response === 2) {
+        app.quit();
+      }
+    });
   }
 });
 
@@ -60,17 +64,23 @@ declare const SETTINGS_WEBPACK_ENTRY: string;
 declare const TRANSLATION_WEBPACK_ENTRY: string;
 declare const CLIPBOARD_WEBPACK_ENTRY: string;
 declare const ICHI_PRELOAD_WEBPACK_ENTRY: string;
+declare const DEEP_WEBPACK_ENTRY: string;
 declare const DEEP_PRELOAD_WEBPACK_ENTRY: string;
 
 ipcMain.on("log", (event, message) => {
   console.log(message);
 });
 
+ipcMain.on('restartProgram', () => {
+  app.relaunch();
+  app.exit();
+});
+
 ipcMain.handle("get/libPath", async (event) => {
-    // Some libraries (like clipboard-event) can't get packaged in order to work (for example because they use executable files)
-    // Those libraries are in the src/lib folder and this function returns its path for both unpackaged and packaged versions
-    // So use this function whenever you use a module from the src/lib path
-    return app.isPackaged ? path.join(process.resourcesPath, 'lib') : path.join(process.cwd(), 'src', 'lib');
+  // Some libraries (like clipboard-event) can't get packaged in order to work (for example because they use executable files)
+  // Those libraries are in the src/lib folder and this function returns its path for both unpackaged and packaged versions
+  // So use this function whenever you use a module from the src/lib path
+  return app.isPackaged ? path.join(process.resourcesPath, 'lib') : path.join(process.cwd(), 'src', 'lib');
 });
 
 
@@ -80,17 +90,17 @@ const showWindowWhenReady = (window: BrowserWindow, shouldShowInProduction: bool
   if (shouldShowInProduction) {
     window.once('ready-to-show', () => { window.show(); });
     return;
-  } 
+  }
 
   // Added for debugging convenience
-  if(process.env.JAPREADER_SHOW_ALL_WINDOWS === "true") {
+  if (process.env.JAPREADER_SHOW_ALL_WINDOWS === "true") {
     window.once('ready-to-show', () => { window.show(); });
     return;
   }
 }
 
 const createReaderWindow = (): BrowserWindow => {
- const readerWindow = new BrowserWindow({
+  const readerWindow = new BrowserWindow({
     height: 600,
     width: 800,
     show: false,
@@ -105,8 +115,32 @@ const createReaderWindow = (): BrowserWindow => {
 
   showWindowWhenReady(readerWindow, true);
 
-  ipcMain.on('sendParsedData', (event, words, fullText) => {
-    readerWindow.webContents.send('receiveParsedData', words, fullText);
+  readerWindow.on('close', (e) => {
+    const choice = dialog.showMessageBoxSync(readerWindow,
+      {
+        type: 'question',
+        buttons: ['Yes', 'No'],
+        title: 'Confirm',
+        message: 'Are you sure you want to quit?'
+      });
+    if (choice == 1) {
+      e.preventDefault();
+    }
+    else {
+      BrowserWindow.getAllWindows().filter(win => win.id != readerWindow.id)
+        .forEach(win => {
+          win.close()
+        })
+      app.exit();
+    }
+  });
+
+  ipcMain.on('tooManyCharacters', () => {
+    readerWindow.webContents.send('tooManyCharacters');
+  });
+
+  ipcMain.on('parseNotification', () => {
+    readerWindow.webContents.send('parseNotification');
   });
 
   ipcMain.on('ichiConnected', () => {
@@ -117,27 +151,39 @@ const createReaderWindow = (): BrowserWindow => {
     readerWindow.webContents.send('ichiConnectionError');
   });
 
+  ipcMain.on('sendParsedData', (event, words, fullText) => {
+    readerWindow.webContents.send('receiveParsedData', words, fullText);
+  });
+
+  ipcMain.on('refreshReader', () => {
+    readerWindow.webContents.send('refreshReader');
+  });
 
   return readerWindow;
 }
 
 const createClipboardWindow = (): BrowserWindow => {
   const clipboardWindow = new BrowserWindow({
-      height: 600,
-      width: 800,
-      show: false,
-      webPreferences: {
-        contextIsolation: false,
-        nodeIntegration: true,
-      },
-    });
+    height: 600,
+    width: 800,
+    show: false,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
 
-    clipboardWindow.loadURL(CLIPBOARD_WEBPACK_ENTRY);
-    clipboardWindow.webContents.openDevTools();
+  clipboardWindow.loadURL(CLIPBOARD_WEBPACK_ENTRY);
+  clipboardWindow.webContents.openDevTools();
 
-    showWindowWhenReady(clipboardWindow, false);
+  showWindowWhenReady(clipboardWindow, false);
 
-    return clipboardWindow;
+  clipboardWindow.on('close', (e) => {
+    e.preventDefault();
+    clipboardWindow.hide();
+  });
+
+  return clipboardWindow;
 }
 
 const createIchiWindow = (): BrowserWindow => {
@@ -158,9 +204,15 @@ const createIchiWindow = (): BrowserWindow => {
 
   showWindowWhenReady(ichiWindow, false);
 
+  ichiWindow.on('close', (e) => {
+    e.preventDefault();
+    ichiWindow.hide();
+  });
+
   ipcMain.on('clipboardChanged', (event, text) => {
     ichiWindow.webContents.send('parseWithIchi', text);
   });
+
 
   return ichiWindow;
 }
@@ -178,16 +230,26 @@ const createDeepWindow = (): BrowserWindow => {
     },
   });
 
-  deepWindow.loadURL('https://www.deepl.com/translator#ja/en/');
+  deepWindow.loadURL(useDeepLApi ? DEEP_WEBPACK_ENTRY : 'https://www.deepl.com/translator#ja/en/');
   deepWindow.webContents.openDevTools();
 
   showWindowWhenReady(deepWindow, false);
+
+  deepWindow.on('close', (e) => {
+    e.preventDefault();
+    deepWindow.hide();
+  });
+
+  ipcMain.on('clipboardChanged', (event, text) => {
+    deepWindow.webContents.send('translateWithDeepL', text);
+  });
+
 
   return deepWindow;
 }
 
 const createTranslationWindow = (): BrowserWindow => {
- const translationWindow = new BrowserWindow({
+  const translationWindow = new BrowserWindow({
     height: 600,
     width: 800,
     show: false,
@@ -200,13 +262,76 @@ const createTranslationWindow = (): BrowserWindow => {
   translationWindow.loadURL(TRANSLATION_WEBPACK_ENTRY);
   translationWindow.webContents.openDevTools();
 
+
+
+  ipcMain.on('fadeText', (event, shouldFade) => {
+    translationWindow.webContents.send('fadeText', shouldFade);
+  });
+
+  ipcMain.on('showTranslation', (event, sourceText, targetText) => {
+    translationWindow.webContents.send(
+      'showTranslation',
+      sourceText,
+      targetText
+    );
+  });
+
+  ipcMain.on('requestTranslation', () => {
+    translationWindow.webContents.send('requestTranslation');
+  });
+
+  ipcMain.on('tooManyCharacters', () => {
+    translationWindow.webContents.send('tooManyCharacters');
+  });
+
+  ipcMain.on('translateNotification', () => {
+    translationWindow.webContents.send('translateNotification');
+  });
+
+  ipcMain.on('deepLConnected', () => {
+    translationWindow.webContents.send('deepLConnected');
+  });
+
+  ipcMain.on('deepLConnectionError', () => {
+    translationWindow.webContents.send('deepLConnectionError');
+  });
+
+  if (!useReader) {
+    translationWindow.on('close', (e) => {
+      const choice = dialog.showMessageBoxSync(translationWindow,
+        {
+          type: 'question',
+          buttons: ['Yes', 'No'],
+          title: 'Confirm',
+          message: 'Are you sure you want to quit?'
+        });
+      if (choice == 1) {
+        e.preventDefault();
+      }
+      else {
+        BrowserWindow.getAllWindows().filter(win => win.id != translationWindow.id)
+          .forEach(win => {
+            win.close()
+          })
+        app.exit();
+      }
+    });
+  }
+  else {
+    translationWindow.on('close', (e) => {
+      e.preventDefault();
+      translationWindow.hide();
+    });
+  }
+
+
   showWindowWhenReady(translationWindow, true);
-  
+
   return translationWindow;
 }
 
 const createDictionaryWindow = (): BrowserWindow => {
- const dictionaryWindow = new BrowserWindow({
+  const dictionaryWindow = new BrowserWindow({
     height: 600,
     width: 800,
     show: false,
@@ -221,11 +346,31 @@ const createDictionaryWindow = (): BrowserWindow => {
 
   showWindowWhenReady(dictionaryWindow, true);
 
+  dictionaryWindow.on('close', (e) => {
+    e.preventDefault();
+    dictionaryWindow.hide();
+  });
+  ipcMain.on('openDict', () => {
+    dictionaryWindow.show();
+  });
+
+  ipcMain.on('hideDict', () => {
+    dictionaryWindow.hide();
+  });
+
+  ipcMain.on('sendWordData', (event, wordData) => {
+    dictionaryWindow.webContents.send('receiveWordData', wordData);
+  });
+
+  ipcMain.on('sendTranslation', (event, englishText) => {
+    dictionaryWindow.webContents.send('receiveTranslation', englishText);
+  });
+
   return dictionaryWindow;
 }
 
 const createSettingsWindow = (): BrowserWindow => {
- const settingsWindow = new BrowserWindow({
+  const settingsWindow = new BrowserWindow({
     height: 600,
     width: 800,
     show: false,
@@ -240,6 +385,30 @@ const createSettingsWindow = (): BrowserWindow => {
 
   showWindowWhenReady(settingsWindow, true);
 
+  settingsWindow.on('close', (event) => {
+    event.preventDefault();
+    settingsWindow.hide();
+  });
+
+  ipcMain.on('openOptions', () => {
+    settingsWindow.show();
+  });
+
+  ipcMain.on('hideOptions', () => {
+    settingsWindow.hide();
+  });
+
+  ipcMain.handle('showDialog', async (e, message) => {
+    const result = dialog.showMessageBox(settingsWindow, {
+      type: 'question',
+      buttons: ['Yes', 'No'],
+      title: 'Confirm',
+      message: message
+    });
+    return result;
+  })
+
+
   return settingsWindow;
 }
 
@@ -249,11 +418,15 @@ const initializeApp = (): void => {
 
   const clipboardWindow = createClipboardWindow();
   const ichiWindow = createIchiWindow();
-  const readerWindow = createReaderWindow();
-  const deepWindow = createDeepWindow();
+  if (useReader) {
+    const readerWindow = createReaderWindow();
+  }
+  if (useDeepL) {
+    const deepWindow = createDeepWindow();
+    const translationWindow = createTranslationWindow();
+  }
   const dictionaryWindow = createDictionaryWindow();
   const settingsWindow = createSettingsWindow();
-  const translationWindow = createTranslationWindow();
 
 };
 
--- src/windows/deep/preload.ts
+++ src/windows/deep/preload.ts
@@ -1,2 +1,96 @@
 import log from 'electron-log/renderer';
 log.silly('Initialized the deep process');
+
+const { ipcRenderer } = require('electron');
+const deepl = require('deepl-node');
+const { useDeepLApi, deepLApiKey } = {"useDeepLApi": false, "deepLApiKey": ""};
+
+if (useDeepLApi) {
+  try {
+    const translator = new deepl.Translator(deepLApiKey);
+  } catch (error) {
+    ipcRenderer.send('deepLConnectionError');
+    console.error(error);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  ipcRenderer.on('translateWithDeepL', (event, text) => {
+    ipcRenderer.send('translateNotification');
+    const currentText = text.replace(/…+/, '…').replace(/・+/g, '…');
+    if (useDeepLApi) {
+      const translator = new deepl.Translator(deepLApiKey);
+      translator
+        .translateText(currentText, 'ja', 'en-US')
+        .then(
+          (result: any) => {
+            ipcRenderer.send('showTranslation', result.text, currentText);
+            return result.text
+          },
+          (error: any) => {
+            ipcRenderer.send('deepLConnectionError');
+            console.error(error);
+          })
+        .then(
+          (result: any) => ipcRenderer.send('appendToHistory', currentText, result),
+          (error: any) => console.error(error)
+        );
+    }
+    else {
+      document.location.href = `https://www.deepl.com/translator#ja/en/${currentText}`;
+    }
+  });
+
+  if (useDeepLApi) {
+    const translator = new deepl.Translator(deepLApiKey, { maxRetries: 1, minTimeout: 2000 });
+    const connectionCheck = setTimeout(() => {
+      translator
+        .getUsage()
+        .then((e: any) => {
+          ipcRenderer.send('deepLConnected');
+          clearInterval(connectionCheck);
+        })
+    }, 500);
+
+    setTimeout(() => {
+      if (deepLApiKey == "") {
+        ipcRenderer.send('deepLConnectionError');
+      } else {
+        translator
+          .getUsage()
+          .catch((err: any) => {
+            ipcRenderer.send('deepLConnectionError');
+          })
+      }
+    }, 8000);
+  }
+  else {
+    const targetNode = document.querySelector('div[aria-labelledby="translation-results-heading"]');
+    const sourceNode = document.querySelector('div[aria-labelledby="translation-source-heading"]');
+    const config = { childList: true };
+    const callback = () => {
+      if (targetNode.textContent) {
+        const deeplText = [...targetNode.children].map(x => x.textContent).join(" ");
+        const japaneseText = [...sourceNode.children].map(x => x.textContent).join(" ");
+        ipcRenderer.send('showTranslation', deeplText, japaneseText);
+        ipcRenderer.send('appendToHistory', japaneseText, deeplText);
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+
+    const connectionCheck = setTimeout(() => {
+      if (document.querySelector('.dl_body').children.length !== 0) {
+        ipcRenderer.send('deepLConnected');
+        clearInterval(connectionCheck);
+      }
+    }, 500);
+
+    setTimeout(() => {
+      if (document.body.children.length === 0) {
+        ipcRenderer.send('deepLConnectionError');
+      }
+    }, 8000);
+  }
+});
