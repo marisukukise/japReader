1a491ff5ffd587325162f555ee852824d037909a
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-07-13 12:58:29 +0200

    Working settings

--- src/windows/settings/front/Settings.tsx
+++ src/windows/settings/front/Settings.tsx
@@ -11,9 +11,9 @@ const windowStore = getWindowStore();
 import { DraggableBar } from '@globals/components/DraggableBar/DraggableBar';
 import ConfigurationDrawer from '@globals/components/ConfigurationDrawer/ConfigurationDrawer';
 import { ConfigurationDrawerCommonSettings } from '@globals/components/ConfigurationDrawer/ConfigurationDrawerCommonSettings';
-import { Button, Checkbox, Input, useToasts } from '@geist-ui/core';
+import { Button, Checkbox as GeistCheckbox, Input as GeistInput, useToasts } from '@geist-ui/core';
 import { IPC_CHANNELS } from '@globals/ts/main/objects';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import { addHideUIListener, setupEffect, toastLayout } from '@globals/ts/renderer/helpers';
 import { ipcRenderer } from 'electron';
 
@@ -24,9 +24,48 @@ import { ipcRenderer } from 'electron';
 // TODO: Make settings work (to save to store and on launch)
 // TODO: Add error checking option
 
+interface InputProps {
+    formData: any;
+    setFormData: React.Dispatch<any>;
+    settingName: string;
+    disabled?: boolean;
+    children?: ReactNode;
+}
+
+const Input = ({ formData, disabled, setFormData, settingName, placeholder, children }: InputProps & { placeholder: string }) => {
+    return <GeistInput
+        disabled={disabled}
+        name={settingName}
+        id={settingName}
+        value={formData[settingName]}
+        placeholder={placeholder}
+        onChange={(e: any) => {
+            setFormData((prevFormData: any) => ({
+                ...prevFormData, [settingName]: e.target.value
+            }));
+        }} >
+        {children}
+    </GeistInput>
+}
+
+const Checkbox = ({ formData, disabled, setFormData, settingName, children }: InputProps) => {
+    return <GeistCheckbox
+        disabled={disabled}
+        name={settingName}
+        id={settingName}
+        checked={formData[settingName]}
+        onChange={(e: any) => {
+            setFormData((prevFormData: any) => ({
+                ...prevFormData, [settingName]: e.target.checked
+            }));
+        }}>
+        {children}
+    </GeistCheckbox>
+}
+
 export const Settings = () => {
-    const [isUIShown, setUIShown] = useState(true);
-    const [formData, setFormData] = useState(settingsStore.get('global_settings'));
+    const [isUIShown, setUIShown] = useState<boolean>(true);
+    const [formData, setFormData] = useState<any>(settingsStore.get('global_settings'));
     const { setToast, removeAll } = useToasts(toastLayout);
 
     const showToast = (text: string | ReactNode, delay: number) => setToast({
@@ -56,14 +95,8 @@ export const Settings = () => {
         resetHistoryStore();
     };
 
-    const handleSettingsChange = (event: any) => {
-        const { name, value } = event.target;
-        setFormData((prevFormData: any) => ({ ...prevFormData, [name]: value }));
-    };
-
-
     const applySettings = () => {
-        log.debug('applied!', formData);
+        settingsStore.set('global_settings', formData);
     };
 
     const bringConfirmationDialog = (message: string, callback: () => void) => {
@@ -110,83 +143,82 @@ export const Settings = () => {
             <h3>Press Apply down below to save options.</h3>
             <h3>The program will be restarted. All your progress will be saved.</h3>
         </div>
-        <div id="all-options">
-            <form onSubmit={
-                () => {
-                    bringConfirmationDialog(
-                        'Are you sure you want to apply settings?\nThis will restart japReader.',
-                        applySettings
-                    );
-                }}>
-                <fieldset id="deep">
-                    <legend>Translation</legend>
-                    <Checkbox checked={formData.useDeepL} onChange={handleSettingsChange}>
-                        Use DeepL translation window
-                    </Checkbox>
-                    <Checkbox checked={formData.useDeepLApi} onChange={handleSettingsChange}>
-                        Use DeepL API
-                    </Checkbox>
-                    <Input placeholder="DeepL API Key" value={formData.deepLApiKey} />
-                </fieldset>
-                <fieldset id="reader">
-                    <legend>Reader</legend>
-                    <Checkbox checked={formData.useReader} onChange={handleSettingsChange}>
-                        Use Reader window
-                    </Checkbox>
-                </fieldset>
-                <fieldset id="anki">
-                    <legend>Anki</legend>
-                    <Checkbox checked={formData.ankiIntegration} onChange={handleSettingsChange}>
-                        Enable Anki integration
-                    </Checkbox>
-                </fieldset>
-                <section id="apply">
-                    <Button htmlType='submit'>Apply</Button>
-                </section>
-            </form>
-        </div>
-        <div id="danger-zone">
-            <summary>Big red buttons</summary>
-            <div id="red-buttons">
+        <div id="settings-wrapper">
+            <fieldset id="deep">
+                <legend>Translation</legend>
+                <Checkbox settingName='useDeepL' formData={formData} setFormData={setFormData}
+                    disabled={!formData.useReader}>Use DeepL translation window</Checkbox>
+                <Checkbox settingName='useDeepLApi' formData={formData} setFormData={setFormData}
+                    disabled={!formData.useDeepL}>Use DeepL API</Checkbox>
+                <Input placeholder="DeepL API Key" settingName='deepLApiKey' formData={formData} setFormData={setFormData}
+                    disabled={!formData.useDeepLApi || !formData.useDeepL} />
+            </fieldset>
+            <fieldset id="reader">
+                <legend>Reader</legend>
+                <Checkbox settingName='useReader' formData={formData} setFormData={setFormData}
+                    disabled={!formData.useDeepL}>Use Reader window</Checkbox>
+            </fieldset>
+            <fieldset id="anki">
+                <legend>Anki</legend>
+                <Checkbox settingName='ankiIntegration' formData={formData} setFormData={setFormData}
+                    disabled={!formData.useReader}>Enable Anki integration</Checkbox>
+            </fieldset>
+        </div >
+        <div id="buttons">
+            <div id="apply">
                 <Button onClick={
                     () => {
                         bringConfirmationDialog(
-                            'Are you sure you want to reset window settings?\nThis will delete all settings like font sizes, window positions etc.',
-                            resetWindowStore
+                            'Are you sure you want to apply settings?\nThis will restart japReader.',
+                            applySettings
                         );
-                    }}>Reset window settings</Button>
-                <Button onClick={
-                    () => {
-                        bringConfirmationDialog(
-                            'Are you sure you want to reset status data?\nThis will delete your entire word database (seen, known, etc.)',
-                            resetStatusDataStore
-                        );
-                    }}>Reset status data</Button>
-                <Button onClick={
-                    () => {
-                        bringConfirmationDialog(
-                            'Are you sure you want to reset global settings to default?\nThis will bring all settings in this window back to default.',
-                            resetSettingsStore
-                        );
-                    }}>Reset global settings</Button>
-                <Button onClick={
-                    () => {
-                        bringConfirmationDialog(
-                            'Are you sure you want to reset history logs?\nThis will delete all saved translation history.',
-                            resetHistoryStore
-                        );
-                    }}>Reset history logs</Button>
-                <Button onClick={
-                    () => {
-                        bringConfirmationDialog(
-                            'Are you sure you want to reset ALL settings to default?',
-                            resetAllStores
-                        );
-                    }}>Reset EVERYTHING</Button>
+                    }}>Apply</Button>
             </div>
+            <section id="danger-zone">
+                <summary>Big red buttons</summary>
+                <div id="red-buttons">
+                    <Button onClick={
+                        () => {
+                            bringConfirmationDialog(
+                                'Are you sure you want to reset window settings?\nThis will delete all settings like font sizes, window positions etc.',
+                                resetWindowStore
+                            );
+                        }}>Reset window settings</Button>
+                    <Button onClick={
+                        () => {
+                            bringConfirmationDialog(
+                                'Are you sure you want to reset status data?\nThis will delete your entire word database (seen, known, etc.)',
+                                resetStatusDataStore
+                            );
+                        }}>Reset status data</Button>
+                    <Button onClick={
+                        () => {
+                            bringConfirmationDialog(
+                                'Are you sure you want to reset global settings to default?\nThis will bring all settings in this window back to default.',
+                                resetSettingsStore
+                            );
+                        }}>Reset global settings</Button>
+                    <Button onClick={
+                        () => {
+                            bringConfirmationDialog(
+                                'Are you sure you want to reset history logs?\nThis will delete all saved translation history.',
+                                resetHistoryStore
+                            );
+                        }}>Reset history logs</Button>
+                    <Button onClick={
+                        () => {
+                            bringConfirmationDialog(
+                                'Are you sure you want to reset ALL settings to default?',
+                                resetAllStores
+                            );
+                        }}>Reset EVERYTHING</Button>
+                </div>
+            </section>
         </div>
-        {isUIShown && <ConfigurationDrawer
-            settings={settings}
-        />}
+        {
+            isUIShown && <ConfigurationDrawer
+                settings={settings}
+            />
+        }
     </>);
 };
