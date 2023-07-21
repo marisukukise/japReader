cb38ef94f7d8f45fc94a9459a6ce093af6ff2560
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-07-21 16:44:38 +0200

    Added debug logs and increased the playwright timeout time

--- tests/base.test.ts
+++ tests/base.test.ts
@@ -17,11 +17,13 @@ let settingsWindow: Page;
 
 
 test.beforeAll(async () => {
+    console.log("Starting test...")
     const startAppResponse = await startApp();
     visibleWindows = startAppResponse.visibleWindows;
     appInfo = startAppResponse.appInfo;
     electronApp = startAppResponse.electronApp;
 
+    console.log("Creating window objects...")
     dictionaryWindow = visibleWindows.filter(e => e.windowName === 'dictionary')[0].page;
     readerWindow = visibleWindows.filter(e => e.windowName === 'reader')[0].page;
     translationWindow = visibleWindows.filter(e => e.windowName === 'translation')[0].page;
@@ -44,6 +46,7 @@ test.beforeAll(async () => {
 });
 
 test.afterAll(async () => {
+    console.log("Finishing up...")
     for (const visibleWindow of visibleWindows) {
         const page = visibleWindow.page;
         const windowName = visibleWindow.windowName;
@@ -55,6 +58,7 @@ test.afterAll(async () => {
 });
 
 test('Reader should get a successful message', async () => {
+    console.log("Testing inside reader...")
     await readerWindow.waitForSelector('.ichi-state-msg.connected')
 
     const text = await readerWindow.$eval('.ichi-state-msg.connected', (el) => el.textContent)
