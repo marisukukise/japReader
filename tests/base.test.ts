552a5e59711b4c7ba54b8f8eb2a3a0f0c8804ac7
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-07-21 15:11:18 +0200

    Created a minimal working test

--- tests/base.test.ts
+++ tests/base.test.ts
@@ -1,34 +1,65 @@
 import { ElectronApplication, Page } from "playwright-core";
 import { test, expect } from "@playwright/test";
 import { startApp } from "./electronHelpers";
-import { ElectronAppInfo } from "electron-playwright-helpers";
+import { ElectronAppInfo, stubAllDialogs, stubDialog, stubMultipleDialogs } from "electron-playwright-helpers";
+import { VisibleWindow } from "./types";
 
 
 
-let appWindow: Page;
+let visibleWindows: VisibleWindow[];
 let appInfo: ElectronAppInfo;
 let electronApp: ElectronApplication;
+
+let dictionaryWindow: Page;
+let readerWindow: Page;
+let translationWindow: Page;
+let settingsWindow: Page;
 
 
 test.beforeAll(async () => {
     const startAppResponse = await startApp();
-    appWindow = startAppResponse.appWindow;
+    visibleWindows = startAppResponse.visibleWindows;
     appInfo = startAppResponse.appInfo;
     electronApp = startAppResponse.electronApp;
+
+    dictionaryWindow = visibleWindows.filter(e => e.windowName === 'dictionary')[0].page;
+    readerWindow = visibleWindows.filter(e => e.windowName === 'reader')[0].page;
+    translationWindow = visibleWindows.filter(e => e.windowName === 'translation')[0].page;
+    settingsWindow = visibleWindows.filter(e => e.windowName === 'settings')[0].page;
+
+    stubMultipleDialogs(electronApp, [
+        {
+            method: 'showMessageBox',
+            value: {
+               response: 0
+            }
+        },
+        {
+            method: 'showMessageBoxSync',
+            value: {
+                response: 0
+            }
+        }
+    ])
 });
 
 test.afterAll(async () => {
-    await appWindow.screenshot({ path: 'screenshots/final-screen.png' });
-    await appWindow.context().close();
-    await appWindow.close();
+    for (const visibleWindow of visibleWindows) {
+        const page = visibleWindow.page;
+        const windowName = visibleWindow.windowName;
+
+        await page.screenshot({path: `screenshots/final-${windowName}.png`});
+
+    }
+
 });
 
-test('test click', async () => {
-    console.log('test click123')
-    console.log(appWindow.title())
-    // Click button.
-    await appWindow.click('text=Click me');
+test('Dictionary window should have h1 title with value "japReader - Dictionary" on startup', async () => {
+    await dictionaryWindow.waitForSelector('h1')
 
-    // Exit app.
-    await electronApp.close();
+    const text = await dictionaryWindow.$eval('h1', (el) => el.textContent)
+    expect(text).toBe('Dictionary')
+
+    const title = await dictionaryWindow.title()
+    expect(title).toBe('japReader - Dictionary')
 });
