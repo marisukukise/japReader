a3cfe5369e6fb5b17e309842479c59a1bd042d30
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-07-24 18:12:05 +0200

    Declare visible windows in the test

--- tests/base.test.ts
+++ tests/base.test.ts
@@ -1,8 +1,13 @@
-import { ElectronApplication, _electron as electron } from "playwright-core";
+import { ElectronApplication, Page, _electron as electron } from "playwright-core";
 import { test, expect } from "@playwright/test";
 import { findLatestBuild, parseElectronApp, stubMultipleDialogs } from "electron-playwright-helpers";
+import { read } from "fs";
 
 let electronApp: ElectronApplication;
+let readerWindow: Page;
+let dictionaryWindow: Page;
+let translationWindow: Page;
+let settingsWindow: Page;
 
 test.beforeAll(async () => {
     const appInfo = parseElectronApp(findLatestBuild());
@@ -27,6 +32,40 @@ test.beforeAll(async () => {
         // },
     });
 
+    const allPages = electronApp.windows();
+    if (allPages.length !== 7) {
+        throw new Error("Electron should have spawned 7 windows")
+    }
+
+     allPages.forEach((page: Page) => {
+        const url = page.mainFrame().url()
+        if (!url.startsWith('file:///')) return;
+
+        const split_url = url.split('/')
+        const windowName = split_url.at(-2)!
+
+        switch (windowName) {
+            case "dictionary": 
+                dictionaryWindow = page;
+                break;
+            case "reader": 
+                readerWindow = page;
+                break;
+            case "translation":
+                translationWindow = page;
+                break;
+            case "settings":
+                settingsWindow = page;
+                break;
+            default:
+        }
+    })
+
+    if (!dictionaryWindow)  throw new Error("Dictionary page is not defined")
+    if (!readerWindow)      throw new Error("Reader page is not defined")
+    if (!translationWindow) throw new Error("Translation page is not defined")
+    if (!settingsWindow)    throw new Error("Settings page is not defined")
+
     stubMultipleDialogs(electronApp, [
         {
             method: 'showMessageBox',
@@ -47,32 +86,13 @@ test.afterAll(async () => {
     await electronApp.close()
 });
 
-test('Main window state', async () => {
-    console.log(electronApp.windows())
-    expect(1).toBe(1)
-//   const page = await electronApp.firstWindow();
-//   const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
-//   const windowState = await window.evaluate(
-//     (mainWindow): Promise<{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}> => {
-//       const getState = () => ({
-//         isVisible: mainWindow.isVisible(),
-//         isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
-//         isCrashed: mainWindow.webContents.isCrashed(),
-//       });
-
-//       return new Promise(resolve => {
-//         /**
-//          * The main window is created hidden, and is shown only when it is ready.
-//          * See {@link ../packages/main/src/mainWindow.ts} function
-//          */
-//         if (mainWindow.isVisible()) {
-//           resolve(getState());
-//         } else mainWindow.once('ready-to-show', () => resolve(getState()));
-//       });
-//     },
-//   );
-
-//   expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
-//   expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
-//   expect(windowState.isDevToolsOpened, 'The DevTools panel was open').toBeFalsy();
+test('Visible windows titles', async () => {
+    const readerTitle = await readerWindow.title()
+    expect(readerTitle).toBe("japReader")
+    const dictionaryTitle = await dictionaryWindow.title()
+    expect(dictionaryTitle).toBe("japReader - Dictionary")
+    const translationTitle = await translationWindow.title()
+    expect(translationTitle).toBe("japReader - Translation")
+    const settingsTitle = await settingsWindow.title()
+    expect(settingsTitle).toBe("japReader - Settings")
 });
