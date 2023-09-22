960501accc0eade37f86a92a06286263b28c0d9d
Marisu Kukise <109931938+marisukukise@users.noreply.github.com>
2023-09-22 21:56:38 +0200

    fix: fix deepl.com website DOM not parsing correctly

--- src/boxes/deepl/script.js
+++ src/boxes/deepl/script.js
@@ -1,115 +1,130 @@
-require('module-alias/register')
-const { ipcRenderer } = require('electron');
+require("module-alias/register");
+const { ipcRenderer } = require("electron");
 
-const tools = require('@tools');
-const Store = require('electron-store')
+const tools = require("@tools");
+const Store = require("electron-store");
 
-const deepl = require('deepl-node')
+const deepl = require("deepl-node");
 const OPTIONS = new Store(tools.getOptionsStoreOptions());
 const HISTORY = new Store(tools.getHistoryLogsOptions());
-const { useDeepLApi, deepLApiKey } = OPTIONS.get('options');
+const { useDeepLApi, deepLApiKey } = OPTIONS.get("options");
 
 if (useDeepLApi) {
   try {
     const translator = new deepl.Translator(deepLApiKey);
   } catch (error) {
-    ipcRenderer.send('deepLConnectionError');
+    ipcRenderer.send("deepLConnectionError");
     console.error(error);
   }
 }
 
 const appendToHistory = (originalText, translation) => {
-  if (typeof translation !== 'string') result = null;
+  if (typeof translation !== "string") result = null;
   const entry = {
-    "timestamp": Date.now(),
-    "japanese": originalText,
-    "translation": translation
+    timestamp: Date.now(),
+    japanese: originalText,
+    translation: translation,
   };
 
-  const list = HISTORY.get('history');
+  const list = HISTORY.get("history");
 
-  if (list) HISTORY.set('history', list.concat(entry));
-  else HISTORY.set('history', [entry]);
-}
+  if (list) HISTORY.set("history", list.concat(entry));
+  else HISTORY.set("history", [entry]);
+};
 
-window.addEventListener('DOMContentLoaded', () => {
-  ipcRenderer.on('translateWithDeepL', (event, text) => {
-    ipcRenderer.send('translateNotification');
-    const currentText = text.replace(/…+/, '…').replace(/・+/g, '…');
-    if (useDeepLApi) {
-      const translator = new deepl.Translator(deepLApiKey);
-      translator
-        .translateText(currentText, 'ja', 'en-US')
-        .then(
-          result => {
-            ipcRenderer.send('showTranslation', result.text, currentText);
-            return result.text
-          },
-          error => {
-            ipcRenderer.send('deepLConnectionError');
-            console.error(error);
-          })
-        .then(
-          result => ipcRenderer.send('appendToHistory', currentText, result),
-          error => console.error(error)
-        );
-    }
-    else {
-      document.location.href = `https://www.deepl.com/translator#ja/en/${currentText}`;
-    }
-  });
-
-  if (useDeepLApi) {
-    const translator = new deepl.Translator(deepLApiKey, { maxRetries: 1, minTimeout: 2000 });
-    const connectionCheck = setTimeout(() => {
-      translator
-        .getUsage()
-        .then(e => {
-          ipcRenderer.send('deepLConnected');
-          clearInterval(connectionCheck);
-        })
-    }, 500);
-
-    setTimeout(() => {
-      if (deepLApiKey == "") {
-        ipcRenderer.send('deepLConnectionError');
-      } else {
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    ipcRenderer.on("translateWithDeepL", (event, text) => {
+      ipcRenderer.send("translateNotification");
+      const currentText = text.replace(/…+/, "…").replace(/・+/g, "…");
+      if (useDeepLApi) {
+        const translator = new deepl.Translator(deepLApiKey);
         translator
-          .getUsage()
-          .catch(err => {
-            ipcRenderer.send('deepLConnectionError');
-          })
+          .translateText(currentText, "ja", "en-US")
+          .then(
+            (result) => {
+              ipcRenderer.send("showTranslation", result.text, currentText);
+              return result.text;
+            },
+            (error) => {
+              ipcRenderer.send("deepLConnectionError");
+              console.error(error);
+            }
+          )
+          .then(
+            (result) =>
+              ipcRenderer.send("appendToHistory", currentText, result),
+            (error) => console.error(error)
+          );
+      } else {
+        document.location.href = `https://www.deepl.com/translator#ja/en/${currentText}`;
       }
-    }, 8000);
+    });
+
+    if (useDeepLApi) {
+      const translator = new deepl.Translator(deepLApiKey, {
+        maxRetries: 1,
+        minTimeout: 2000,
+      });
+      const connectionCheck = setTimeout(() => {
+        translator.getUsage().then((e) => {
+          ipcRenderer.send("deepLConnected");
+          clearInterval(connectionCheck);
+        });
+      }, 500);
+
+      setTimeout(() => {
+        if (deepLApiKey == "") {
+          ipcRenderer.send("deepLConnectionError");
+        } else {
+          translator.getUsage().catch((err) => {
+            ipcRenderer.send("deepLConnectionError");
+          });
+        }
+      }, 8000);
+    } else {
+      const connectionCheck = setTimeout(() => {
+        if (document.querySelector('section[aria-labelledby="text-translator-section-heading"]')) {
+          ipcRenderer.send("deepLConnected");
+          clearInterval(connectionCheck);
+        }
+      }, 500);
+
+      setTimeout(() => {
+        if (document.body.children.length === 0) {
+          ipcRenderer.send("deepLConnectionError");
+        }
+      }, 8000);
+
+      try {
+        const targetNode = document.body.querySelector(
+          'div[aria-labelledby="translation-target-heading"]'
+        );
+        const sourceNode = document.body.querySelector(
+          'div[aria-labelledby="translation-source-heading"]'
+        );
+        console.log("e");
+        console.log(targetNode, sourceNode)
+
+        const config = { childList: true };
+        const callback = () => {
+          if (targetNode.textContent) {
+            const deeplText = [...targetNode.children]
+              .map((x) => x.textContent)
+              .join(" ");
+            const japaneseText = [...sourceNode.children]
+              .map((x) => x.textContent)
+              .join(" ");
+            ipcRenderer.send("showTranslation", deeplText, japaneseText);
+            ipcRenderer.send("appendToHistory", japaneseText, deeplText);
+          }
+        };
+
+        const observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+      } catch (error) {
+        ipcRenderer.send("deepLConnectionError");
+      }
+    }
   }
-  else {
-    const targetNode = document.querySelector('div[aria-labelledby="translation-results-heading"]');
-    const sourceNode = document.querySelector('div[aria-labelledby="translation-source-heading"]');
-    const config = { childList: true };
-    const callback = () => {
-      if (targetNode.textContent) {
-        const deeplText = [...targetNode.children].map(x => x.textContent).join(" ");
-        const japaneseText = [...sourceNode.children].map(x => x.textContent).join(" ");
-        ipcRenderer.send('showTranslation', deeplText, japaneseText);
-        ipcRenderer.send('appendToHistory', japaneseText, deeplText);
-      }
-    };
-
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-
-    const connectionCheck = setTimeout(() => {
-      if (document.querySelector('.dl_body').length !== 0) {
-        ipcRenderer.send('deepLConnected');
-        clearInterval(connectionCheck);
-      }
-    }, 500);
-
-    setTimeout(() => {
-      if (document.body.children.length === 0) {
-        ipcRenderer.send('deepLConnectionError');
-      }
-    }, 8000);
-  }
-});
-
+};
--- src/boxes/translation/script.js
+++ src/boxes/translation/script.js
@@ -1,55 +1,59 @@
-require('module-alias/register')
+require("module-alias/register");
 
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
-const tools = require('@tools');
+const tools = require("@tools");
 
-const Store = require('electron-store')
+const Store = require("electron-store");
 
 const OPTIONS = new Store(tools.getOptionsStoreOptions());
 
+let englishText = "";
+let japaneseText = "";
 
-let englishText = '';
-let japaneseText = '';
-
-
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   // eslint-disable-next-line global-require
-  const $ = require('jquery');
+  const $ = require("jquery");
 
   let deepLConnected = false;
   let deepLDual = false;
   let onTop = false;
   let showBorders = true;
 
-  ipcRenderer.send('readyTranslation');
+  ipcRenderer.send("readyTranslation");
 
-  const { translationFontSize, useDeepLApi, fontFamily, translationTransparent, darkMode } = OPTIONS.get('options')
+  const {
+    translationFontSize,
+    useDeepLApi,
+    fontFamily,
+    translationTransparent,
+    darkMode,
+  } = OPTIONS.get("options");
   if (darkMode) {
-    document.documentElement.classList.add('dark-mode');
+    document.documentElement.classList.add("dark-mode");
   }
-  document.querySelector('#app').style.fontSize = `${translationFontSize}pt`;
-  document.querySelector('#app').style.fontFamily = `${fontFamily}`;
+  document.querySelector("#app").style.fontSize = `${translationFontSize}pt`;
+  document.querySelector("#app").style.fontFamily = `${fontFamily}`;
 
-  $(window).on('keydown', (e) => {
+  $(window).on("keydown", (e) => {
     switch (e.key) {
-      case 'o':
-        ipcRenderer.send('openOptions');
+      case "o":
+        ipcRenderer.send("openOptions");
         break;
-      case 's':
-        onTop = tools.toggle_onTop(onTop, $('body'));
-        ipcRenderer.send('translationOnTop');
+      case "s":
+        onTop = tools.toggle_onTop(onTop, $("body"));
+        ipcRenderer.send("translationOnTop");
         break;
-      case 'h':
+      case "h":
         if (translationTransparent) {
           if (showBorders) {
             showBorders = false;
-            document.body.style.borderStyle = 'hidden';
-            document.querySelector('#move-bar').style.visibility = 'hidden';
+            document.body.style.borderStyle = "hidden";
+            document.querySelector("#move-bar").style.visibility = "hidden";
           } else {
             showBorders = true;
-            document.body.style.borderStyle = 'dashed';
-            document.querySelector('#move-bar').style.visibility = 'visible';
+            document.body.style.borderStyle = "dashed";
+            document.querySelector("#move-bar").style.visibility = "visible";
           }
         }
         break;
@@ -57,79 +61,105 @@ window.addEventListener('DOMContentLoaded', () => {
     return true;
   });
 
-  const options = OPTIONS.get('options')
+  const options = OPTIONS.get("options");
   deepLDual = options.deepLDual;
 
-  $('#app').html(
-    `Connecting to ${useDeepLApi ? "DeepL API" : '<span class="url">https://www.deepl.com/</span>'}.`
+  $("#app").html(
+    `Connecting to ${
+      useDeepLApi
+        ? "DeepL API"
+        : '<span class="url">https://www.deepl.com/</span>'
+    }.`
   );
-  $('#app').append('<br>');
-  $('#app').append('Please wait patiently...');
+  $("#app").append("<br>");
+  $("#app").append("Please wait patiently...");
 
-  ipcRenderer.on('deepLConnected', () => {
+  ipcRenderer.on("deepLConnected", () => {
     if (!deepLConnected) {
       deepLConnected = true;
-      $('#app').html(
-        `Successfully connected to ${useDeepLApi ? "DeepL API" : '<span class="url">https://www.deepl.com/</span>'}.`
+      $("#app").html(
+        `Successfully connected to ${
+          useDeepLApi
+            ? "DeepL API"
+            : '<span class="url">https://www.deepl.com/</span>'
+        }.`
       );
-      $('#app').append('<br>');
-      $('#app').append('Copy Japanese text to get DeepL translations.');
+      $("#app").append("<br>");
+      $("#app").append("Copy Japanese text to get DeepL translations.");
       if (useDeepLApi) {
-        $('#app').append('<br><br>');
-        $('#app').append("You're using DeepL API which has a monthly limit of 500,000 characters. <br> To have unlimited translation consider disabling DeepL API in the options menu.");
+        $("#app").append("<br><br>");
+        $("#app").append(
+          "You're using DeepL API which has a monthly limit of 500,000 characters. <br> To have unlimited translation consider disabling DeepL API in the options menu."
+        );
       }
     }
   });
 
-  ipcRenderer.on('deepLConnectionError', () => {
-    $('#app').html(
-      `Unable to connect to ${useDeepLApi ? "DeepL API" : '<span class="url">https://www.deepl.com/</span>'}.`
+  ipcRenderer.on("deepLConnectionError", () => {
+    $("#app").html(
+      `Unable to connect to ${
+        useDeepLApi
+          ? "DeepL API"
+          : '<span class="url">https://www.deepl.com/</span>'
+      }.`
     );
-    $('#app').append('<br>');
-    $('#app').append(
-      `Check your internet connection, and make sure ${useDeepLApi ? "your API key is correct" : "the site is up"}.`
+    $("#app").append("<br>");
+    $("#app").append(
+      `Check your internet connection, and make sure ${
+        useDeepLApi ? "your API key is correct" : "the site is up"
+      }.`
     );
-    $('#app').append('<br><br>');
-    $('#app').append(
-      `Once you ${useDeepLApi ? "confirm your API key" : "are able to connect to the site"}, restart this program.`
+    $("#app").append("<br><br>");
+    $("#app").append(
+      `Once you ${
+        useDeepLApi ? "confirm your API key" : "are able to connect to the site"
+      }, restart this program.`
     );
-    $('#app').append('<br>');
-    $('#app').append('All of your progress will be saved.');
-  });
-
-  ipcRenderer.on('translateNotification', () => {
-    $('#app').html('Translating...');
-  });
-
-  ipcRenderer.on('fadeText', (event, shouldFade) => {
-    if (shouldFade) $('#app').addClass('fade');
-    else $('#app').removeClass('fade');
-  });
-
-  ipcRenderer.on('tooManyCharacters', () => {
-    $('#app').html('Too many characters copied to clipboard...');
-    $('#app').append('<br>');
-    $('#app').append(
-      `No request has been made to ${useDeepLApi ? "DeepL API" : '<span class="url">https://www.deepl.com/</span>'}.`
-    );
-    $('#app').append('<br>');
-    $('#app').append(
-      'This has been implemented to prevent you from being banned.'
+    $("#app").append("<br>");
+    $("#app").append("All of your progress will be saved.");
+    $("#app").append("<br>");
+    $("#app").append(
+      "(It is also possible that deepl.com has changed something on its website and it broke japReader, in which case you can open an issue on github or temporarily enable a DeepL API Key in options)"
     );
   });
 
-  ipcRenderer.on('showTranslation', (event, sourceText, targetText) => {
-    englishText = sourceText.replace(/"/g, '');
+  ipcRenderer.on("translateNotification", () => {
+    $("#app").html("Translating...");
+  });
+
+  ipcRenderer.on("fadeText", (event, shouldFade) => {
+    if (shouldFade) $("#app").addClass("fade");
+    else $("#app").removeClass("fade");
+  });
+
+  ipcRenderer.on("tooManyCharacters", () => {
+    $("#app").html("Too many characters copied to clipboard...");
+    $("#app").append("<br>");
+    $("#app").append(
+      `No request has been made to ${
+        useDeepLApi
+          ? "DeepL API"
+          : '<span class="url">https://www.deepl.com/</span>'
+      }.`
+    );
+    $("#app").append("<br>");
+    $("#app").append(
+      "This has been implemented to prevent you from being banned."
+    );
+  });
+
+  ipcRenderer.on("showTranslation", (event, sourceText, targetText) => {
+    englishText = sourceText.replace(/"/g, "");
     japaneseText = targetText;
 
-    if (!deepLDual) $('#app').html(englishText);
+    if (!deepLDual) $("#app").html(englishText);
     else {
-      $('#app').html(`<div id="jap-text">${japaneseText}</div>`);
-      $('#app').append(`<div id="english-text">${englishText}</div>`);
+      $("#app").html(`<div id="jap-text">${japaneseText}</div>`);
+      $("#app").append(`<div id="english-text">${englishText}</div>`);
     }
   });
 
-  ipcRenderer.on('requestTranslation', () => {
-    ipcRenderer.send('sendTranslation', englishText);
+  ipcRenderer.on("requestTranslation", () => {
+    ipcRenderer.send("sendTranslation", englishText);
   });
 });
